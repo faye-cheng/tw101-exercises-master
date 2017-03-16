2d9c56f412edc7a0be5bb341525ae5d936596bd3
Faye Cheng <fayehcheng@gmail.com>
2017-03-15 19:26:41 -0700

    Deleted build folder

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    int total = 0;
+
+    void increment() {
+        total++;
+    }
+
+    int total() {
+        return total;
+    }
 }
