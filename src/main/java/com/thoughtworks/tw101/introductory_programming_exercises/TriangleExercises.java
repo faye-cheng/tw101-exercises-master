198335aa6b3813d0a15770124673690cd825446a
Faye Cheng <fayehcheng@gmail.com>
2017-04-11 16:22:24 -0700

    Intro Programming Exercises: PrimeFactors and Triangle

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/TriangleExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/TriangleExercises.java
@@ -12,14 +12,20 @@ public class TriangleExercises {
 //    Print one asterisk to the console.
 //    Example: *
     private static void drawAnAsterisk() {
-
+        System.out.println("Draw an asterisk:");
+        System.out.println("*");
+        System.out.println();
     }
 
 //    Draw a horizontal line
 //    Given a number n, print n asterisks on one line.
 //    Example when n=8:  ********
     private static void drawAHorizontalLine(int n) {
-
+        System.out.println("Draw a horizontal line:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.println("\n");
     }
 
 //    Draw a vertical line
@@ -29,7 +35,11 @@ public class TriangleExercises {
 //            *
 //            *
     private static void drawAVerticalLine(int n) {
-
+        System.out.println("Draw a vertical line:");
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+        System.out.println("");
     }
 
 //    Draw a right triangle
@@ -39,6 +49,14 @@ public class TriangleExercises {
 //            **
 //            ***
     private static void drawARightTriangle(int n) {
-
+        System.out.println("Draw a right triangle:");
+        int numAsterisks = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < numAsterisks; j++) {
+                System.out.print("*");
+            }
+            numAsterisks++;
+            System.out.println("");
+        }
     }
 }
